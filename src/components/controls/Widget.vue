c3e8849e0dd0c1432b4e8150d5c56ab92e8c62f3
Jacky Wang <spizs803@gmail.com>
2017-06-07 22:55:10 +0800

    add widget

---
+++ src/components/controls/Widget.vue
@@ -0,0 +1,18 @@
+<template>
+  <div  class="widget">
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@import  '../../styles/var.scss';
+.widget {
+    background-color: #eee;
+    border-radius: 10%;
+}
+</style>
